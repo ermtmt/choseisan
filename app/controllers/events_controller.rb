52f37ac1dfe6b756f733e1c4e6989172692911e7
ermtmt <xe.mtmt@gmail.com>
2015-10-18 14:39:12 +0900

    不要な外部結合を除去（Event : Tagging : Tag）

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,8 +10,7 @@ class EventsController < ApplicationController
   before_action :check_created_events_count, only: [:new, :create]
 
   def index
-    # @events = Event.filter_events(current_user, @filter_tags).eager_load(:event_entries, :tags).order(id: :desc).page(params[:page])
-    @events = current_user.related_events.filter_tags(@filter_tags).eager_load(:event_entries, :tags).order(id: :desc).page(params[:page])
+    @events = current_user.related_events.filter_tags(@filter_tags).eager_load(:event_entries).order(id: :desc).page(params[:page])
   end
 
   def show
